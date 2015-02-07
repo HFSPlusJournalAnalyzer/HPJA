e5898b9e26535a4e01315be7620a792529dac1fc
biscuit03 <biscuit@kaist.ac.kr>
2015-02-07 20:32:26 +0900

    biscuit_fixed
    - Structures of HFS+ journal are structed using namedtuple.
    - get-functions of each structures are also implemented.

---
+++ biscuit/HFSPlus_getInstance.py
@@ -0,0 +1,158 @@
+from struct import *
+import HFSPlus_sStructure as ss
+
+def getJournalHeader(jh_binary):
+    endian = unpack_from(">I",jh_binary, 4)[0]
+    eflag = [">","<"][endian == 0x78563412]
+    vec = unpack_from(eflag + 'IIQQQIII',jh_binary)
+    return ss.JournalHeader(*vec)
+
+def getBlockInfo(bi_binary):
+    vec = unpack('<QII',bi_binary)
+    return ss.BlockInfo(*vec)
+
+def getBlockInfoHeader(bih_binary):
+    bih_0 = bih_binary[:16]
+    bih_1 = bih_binary[16:] 
+    vec = list(unpack('<HHIII',bih_0))
+    vec.append(getBlockInfo(bih_1))
+    return ss.BlockInfoHeader(*vec)
+
+def getNodeDescriptor(nd_binary):
+    vec = unpack('>IIbBHH',nd_binary)
+    return ss.NodeDescriptor(*vec)
+
+def getBTHeaderRec(hr_binary):
+    vec = unpack_from('>HIIIIHHIIHIBBI',hr_binary)  # reserved3 field omited
+    return ss.BTHeaderRec(*vec)
+
+def getExtentDescriptor(ed_binary):
+    vec = unpack('>II',ed_binary)
+    return ss.ExtentDescriptor(*vec)
+
+def getForkData(fd_binary):
+    fd_0 = fd_binary[:16]
+    ext = []
+    for i in range(8):
+        temp = fd_binary[16+8*i:16+8*(i+1)]
+        e = getExtentDescriptor(temp)
+        ext.append(e)
+    vec = list(unpack_from('>QII',fd_binary))
+    vec.append(ext)
+    return ss.ForkData(*vec)
+
+def getCatalogKey(ck_binary):
+    parID, nameLen = unpack_from(">IH", ck_binary)
+    nameStr = ck_binary[6:]
+    nodeUnicode = "".join(map(unichr, unpack(">"+nameLen*"H", nameStr)))
+    return ss.CatalogKey(parID, [nameLen, nodeUnicode])
+
+def getBSDInfo(bsd_binary):
+    vec = unpack(">IIBBHI", bsd_binary)
+    return ss.BSDInfo(*vec)
+
+def getFolderInfo(foi_binary):
+    RectVec = unpack_from(">hhhh", foi_binary)
+    Flag = unpack_from(">H", foi_binary, 8)[0]
+    Location = unpack_from(">hh", foi_binary, 10)
+    reserved = unpack_from(">H", foi_binary, 14)[0]
+    vec = [ss.Rect(*RectVec), Flag, ss.Point(*Location), reserved]
+    return ss.FolderInfo(*vec)
+
+def getExtendedFolderInfo(efi_binary):
+    scrPosVec = unpack_from(">hh", efi_binary)
+    vec = list(unpack_from(">iHhi", efi_binary, 4))
+    vec.insert(0, ss.Point(*scrPosVec))
+    return ss.ExtendedFolderInfo(*vec)
+
+def getFileInfo(fii_binary):
+    vec = list(unpack_from(">IIH",fii_binary))
+    Location = unpack_from(">hh", fii_binary, 10)
+    res = unpack_from(">H", fii_binary, 14)[0]
+    vec.extend([ss.Point(*Location), res])
+    return ss.FileInfo(*vec)
+
+def getExtendedFileInfo(efi_binary):
+    res1 = unpack_from(">hhhh", efi_binary)
+    vec = list(unpack_from(">Hhi", efi_binary, 8))
+    vec.insert(0, res1)
+    return ss.ExtendedFileInfo(*vec)
+
+def getCatalogFolder(cfo_binary):
+    cfo_0 = cfo_binary[:32]   
+    cfo_1 = cfo_binary[32:48]  # BSDInfo
+    cfo_2 = cfo_binary[48:64]  # userInfo
+    cfo_3 = cfo_binary[64:80]  # finderInfo
+    cfo_4 = cfo_binary[80:]  # textEncoding, reserved 
+    
+    vec0 = list(unpack('>hHIIIIIII',cfo_0))
+    BSDInfo = getBSDInfo(cfo_1)
+    userInfo = getFolderInfo(cfo_2)
+    finderInfo = getExtendedFolderInfo(cfo_3)
+    textEncoding, reserved = unpack(">II", cfo_4)
+    vec1 = [BSDInfo, userInfo, finderInfo, textEncoding, reserved]
+    
+    vec0.extend(vec1)
+    return ss.CatalogFolder(*vec0)
+    
+def getCatalogFile(cfi_binary):
+    cfi_0 = cfi_binary[:32]
+    cfi_1 = cfi_binary[32:48]  # BSDInfo
+    cfi_2 = cfi_binary[48:64]  # userInfo
+    cfi_3 = cfi_binary[64:80]  # finderInfo
+    cfi_4 = cfi_binary[80:88]  # textEncoding, reserved 
+    cfi_5 = cfi_binary[88:168] # dataFork
+    cfi_6 = cfi_binary[168:]   # resourceFork
+    
+    vec0 = list(unpack('>hHIIIIIII',cfi_0))
+    BSDInfo = getBSDInfo(cfi_1)
+    userInfo = getFileInfo(cfi_2)
+    finderInfo = getExtendedFileInfo(cfi_3)
+    textEncoding, reserved2 = unpack(">II", cfi_4) 
+    dataFork = getForkData(cfi_5)
+    resourceFork = getForkData(cfi_6)
+    vec1 = [BSDInfo, userInfo, finderInfo, textEncoding, reserved2, dataFork, resourceFork]
+    
+    vec0.extend(vec1)
+    return ss.CatalogFile(*vec0)
+
+def getCatalogThread(cth_binary):
+    cth_0 = cth_binary[:10]
+    cth_1 = cth_binary[10:]
+    recordType, reserved, parID, nameLen = unpack_from(">hhIH",cth_binary)
+    nodeUnicode = "".join(map(unichr, unpack(">"+nameLen*"H", cth_1)))
+    
+    return ss.CatalogThread(recordType, reserved, parID, [nameLen, nodeUnicode])
+    
+def getCatalogLeaf(clk_binary):
+    keyLen = unpack_from(">H", clk_binary)[0]
+    catalKey = getCatalogKey(clk_binary[2:2+keyLen])
+    Key = ss.BTKeyedRec(keyLen, catalKey) 
+    rec_binary = clk_binary[len(Key):]
+    recordType = unpack_from(">H", rec_binary)[0]
+    typeList = [0, getCatalogFolder, getCatalogFile, getCatalogThread, getCatalogThread]
+    Record = typeList[recordType](rec_binary)
+    return ss.CatalogLeafRec(Key, Record)
+    
+def getVolumeHeader(vh_binary):
+    vh_0 = vh_binary[:80]
+    vh_1 = vh_binary[80:112]
+    vh_sp = []
+    for i in range(5):
+        t = vh_binary[112+80*i:112+80*(i+1)]
+        vh_sp.append(t)
+        
+    vec = list(unpack('>HHIIIIIIIIIIIIIIIIIQ',vh_0))
+    
+    fI = unpack('>IIIIIIII',vh_1)
+    vec.append(fI)
+    
+    for i in range(5):
+        vec.append(getForkData(vh_sp[i]))
+    return ss.VolumeHeader(*vec)
+    
+f = open(r"C:\Users\user\Desktop\Untitled2", 'rb')
+s = f.read()
+t = getCatalogLeaf(s)
+print getCatalogLeaf(s)
+    
---
+++ biscuit/HFSPlus_sStructure.py
@@ -0,0 +1,70 @@
+from collections import namedtuple
+
+JournalHeader = namedtuple("JournalHeader", ['magic','endian','start','end','size','blhdr_size','checksum','jhdr_size'])
+BlockInfoHeader = namedtuple("BlockInfoHeader", ['max_blocks','num_blocks','bytes_used','checksum','pad','binfo0'])
+BlockInfo = namedtuple("BlockInfo", ['bnum', 'bsize', 'next'])
+NodeDescriptor = namedtuple("NodeDescriptor", ['fLink','bLink','kind','height','numRecords','reserved'])
+BTHeaderRec = namedtuple("BTHeaderRec", ['treeDepth','rootNode',
+                                        'leafRecords','firstLeafNode','lastLeafNode','nodeSize',
+                                        'maxKeyLength','totalNodes','freeNodes','reserved1',
+                                        'clumpSize','btreeType',
+                                        'keyCompareType','attributes'])
+VolumeHeader = namedtuple("VolumeHeader", ['signature','version','attributes','lastMountedVersion','journalInfoBlock',
+                                          'createDate','modifyDate','backupDate','checkedDate',
+                                          'fileCount','folderCount',
+                                          'blockSize','totalBlocks','freeBlocks','nextAllocation',
+                                          'rsrcClumpSize','dataClumpSize',
+                                          'nextCatalogID','writeCount',
+                                          'encodingsBitmap','finderInfo',
+                                          'allocationFile', 'extentsFile', 'catalogFile', 'attributesFile', 'startupFile'])
+
+'''
+ Leaf Records  
+'''
+
+ExtentDescriptor = namedtuple("ExtentDescriptor", ['startBlock','blockCount'])
+ForkData = namedtuple("ForkData", ['logicalSize','clumpSize','totalBlocks','extents'])
+sBTKeyedRec = namedtuple("BTKeyedRec", ['keyLength','key'])
+
+class BTKeyedRec(sBTKeyedRec):
+    def __len__(self):
+        return 2 + len(self.key)
+
+BSDInfo = namedtuple("BSDInfo", ['ownerID', 'groupID', 'adminFlags', 'ownerFlags', 'fileMode', 'special'])
+
+'''
+Finder
+'''
+Point = namedtuple('Point', ["v", "h"])
+Rect = namedtuple('Rect', ['top', 'left', 'bottom', 'right'])
+FileInfo = namedtuple('FileInfo', ['fileType', 'fileCreator', 'finderFlags', 'location', 'reservedField'])
+ExtendedFileInfo = namedtuple("ExtendedFileInfo", ['reserved1', 'extendedFinderFlags', 'reserved2', 'putAwayFolderID'])
+FolderInfo = namedtuple('FolderInfo', ['windowBounds', 'finderFlags', 'location', 'reservedField'])
+ExtendedFolderInfo = namedtuple("ExtendedFolderInfo", ['scrollPosition', 'reserved1', 'extendedFinderFlags', 'reserved2', 'putAwayFolderID'])
+
+'''
+Catalog Records 
+'''
+CatalogLeafRec = namedtuple("CatalogLeafRec", ['key', 'record'])
+
+sCatalogKey = namedtuple("CatalogKey", ["parentID", "nodeName"])
+
+class CatalogKey(sCatalogKey):
+    def __len__(self):
+        return 4+2+2*self.nodeName[0]
+
+CatalogFolder = namedtuple('CatalogFolder', ['recordType', 'flags', 
+                                             'valence', 'folderID', 
+                                             'createDate', 'contentModDate', 'attributeModDate', 'accessDate', 'backupDate', 
+                                             'permissions','userInfo','finderInfo',
+                                             'textEncoding', 'reserved'])
+
+CatalogFile = namedtuple('CatalogFolder', ['recordType', 'flags', 
+                                             'reserved1', 'fileID', 
+                                             'createDate', 'contentModDate', 'attributeModDate', 'accessDate', 'backupDate', 
+                                             'permissions','userInfo','finderInfo',
+                                             'textEncoding', 'reserved2',
+                                             'dataFork', 'resourceFork'])
+
+CatalogThread = namedtuple("CatalogThread", ['recordType', 'reserved', 'parentID', 'nodeName'])
+
