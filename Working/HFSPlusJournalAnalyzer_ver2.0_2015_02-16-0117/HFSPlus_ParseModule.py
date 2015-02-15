4818d1ee21ef0a28bf48bc945a04b8ff9b9e307c
hyunhochoi <factorial__@naver.com>
2015-02-16 01:17:39 +0900

    2015-02-16 01:17

---
+++ Working/HFSPlusJournalAnalyzer_ver2.0_2015_02-16-0117/HFSPlus_ParseModule.py
@@ -0,0 +1,194 @@
+
+'''
+Created on 2015. 2. 8.
+
+@author: biscuit
+'''
+'''
+from HFSPlus_GetInstance import *
+
+def journalBufferParser(endian,journalBuffer):
+
+    blhdrPointer=base
+    bnext=0
+    tranNum=0
+    blockNum=0
+    tranBlocks=[0]
+
+    while not VerifyChecksum('blhdr',endian,journalBuffer[blhdrPointer%size:(blhdrPointer+32)%size],32):
+        blhdrPointer+=jhdr_size
+
+    while blhdrPointer<base+size:
+
+        if bnext==0:
+            tranNum+=1
+            blockNum=0
+            
+        max_blocks,num_blocks,bytes_used,checksum,pad,temp1,bnext=unpack_from(endian+'HHLLLQL',journalBuffer,blhdrPointer%size)
+
+        blPointer=blhdrPointer+blhdr_size
+        blhdrPointer+=32
+
+        for i in range(1,num_blocks):
+
+            blockNum+=1
+            tranBlocks[0]+=1
+            
+            bnum,bsize=unpack_from(endian+'LL',journalBuffer,blhdrPointer%size)
+                
+            if blPointer<size and blPointer+bsize>=size:
+                tranBlocks.append(RawBlock(blPointer,tranNum,blockNum,bnum,bsize,jhdr_size,journalBuffer[blPointer:size]+journalBuffer[0:(blPointer+bsize)%size]))
+                            
+            else:
+                tranBlocks.append(RawBlock(blPointer,tranNum,blockNum,bnum,bsize,jhdr_size,journalBuffer[blPointer%size:(blPointer+bsize)%size]))
+            
+            blPointer+=bsize
+
+            blhdrPointer+=16
+        
+        blhdrPointer+=bytes_used-(num_blocks+1)*16
+            
+    return tranBlocks
+'''
+
+'''
+Created on 2015. 2. 8.
+
+@author: biscuit
+'''
+from HFSPlus_GetInstance import *
+
+sect_size = 0x200  # variable for storing a sector size 
+blockMag = 8  # variable for storing a magnification of block from sect_size (blockSize/sect_size)
+sfLoc = {}  # dict for storing the location of special files
+
+def journalParser(journal_blob):
+    global sect_size, blockMag, sfLoc
+    vh_samInd = journal_blob.find("H+")
+    jnl = memoryview(journal_blob)
+    j_header = getJournalHeader(jnl)
+    
+    sect_size = j_header.jhdr_size
+    vh = getVolumeHeader(journal_blob[vh_samInd:vh_samInd+sect_size])
+    sfLoc['allocationFile'] = vh.allocationFile.extents
+    sfLoc['extentsFile'] = vh.extentsFile.extents
+    sfLoc['catalogFile'] = vh.catalogFile.extents
+    sfLoc['attributesFile'] = vh.attributesFile.extents
+    blockMag = vh.blockSize/sect_size
+        
+    j_buf = jnl[sect_size:]
+    
+    startOffset = getStart(j_buf, j_header.end) # setting the startOffset
+    
+    j_ParseList = journalBufferParser(j_buf, j_header, startOffset, [])
+    j_ParseList.insert(0, j_header)
+    
+    return j_ParseList
+
+def getStart(j_buf, end):
+    cur = end
+    lenBuf = len(j_buf)
+    curLine = j_buf[cur:cur+0x10]
+    while curLine != 'Z'*16:
+        cur = (cur+0x10) % lenBuf
+        curLine = j_buf[cur:cur+0x10]
+    while(True):
+        cur = (cur-0x10) % lenBuf
+        curLine = j_buf[cur:cur+0x10]
+        if cur == end:
+            while curLine != 'Z'*16:
+                cur = (cur+0x10) % lenBuf
+                curLine = j_buf[cur:cur+0x10]
+            while curLine == 'Z'*16:
+                cur = (cur+0x10) % lenBuf
+                curLine = j_buf[cur:cur+0x10]
+            while curLine != 'Z'*16:
+                cur = (cur+0x10) % lenBuf
+                curLine = j_buf[cur:cur+0x10]
+            cur = (cur-0x10) % lenBuf
+            curLine = j_buf[cur:cur+0x10]
+        if curLine[4:8] != '\x00\x00\x00\x00':
+            return cur
+        
+
+def journalBufferParser(j_buffer, JournalHeader, startOffset, parseList):
+    global sect_size
+    if startOffset == (JournalHeader.end - sect_size): # -0x200 for Journal Header area
+        return parseList
+    blh = getBlockListHeader(j_buffer[startOffset:])
+    
+    nextOffset = ( startOffset + blh.bytes_used ) % len(j_buffer)
+    if nextOffset - startOffset != blh.bytes_used:
+        curTrans = memoryview( j_buffer[startOffset:].tobytes() + j_buffer[:nextOffset].tobytes() )
+    else: curTrans = j_buffer[startOffset:nextOffset]
+    
+    parseList.append(transParser(curTrans))
+    
+    return journalBufferParser(j_buffer, JournalHeader, nextOffset, parseList)
+
+def transParser(trans):
+    blh = getBlockListHeader(trans)  # block list header
+    bi_List = []  # list of block info's 
+    data_offset = 0  # for computing the offset from the bottom of the transaction block, of the data-area.
+    for i in range(blh.num_blocks-1):
+        bi = getBlockInfo(trans[0x20+0x10*i:])
+        data_offset += bi.bsize
+        bi_List.append(bi)
+    
+    data_List = []
+    data_area = trans[-data_offset:]
+    for b in bi_List:
+        data_List.append(getDataBlock(data_area[:b.bsize], b))
+        data_area = data_area[b.bsize:]
+    
+    return [blh, bi_List, data_List]
+
+# auxiliary function ; To identify a data block.
+def getDataBlock(data_block, BlockInfo):
+    global sect_size, sfLoc, blockMag
+    
+    data_sNum = BlockInfo.bnum / blockMag
+    
+    curSType = ""
+    for s in sfLoc:
+        for e in sfLoc[s]:
+            if e.isIn(data_sNum):
+                curSType = s
+                break
+        if curSType != "":
+            break
+        
+    raw_data = data_block.tobytes()
+    if "H+\x00\x04" in raw_data:
+        vh_off = raw_data.find("H+\x00\x04")
+        return getVolumeHeader(data_block[vh_off:vh_off+0x200])
+    if curSType == 'allocationFile':
+        return data_block
+     
+    kindDict = {'catalogFile': [getCatalogLeaf, getCatalogIndex],
+                'extentsFile': [getExtentsLeaf, getExtentsIndex],
+                'attributesFile': [getAttributesLeaf, getAttributesIndex] }
+    
+    kindList = kindDict[curSType]
+    kindList.extend([getHeaderNode, getMapNode])
+    kind = unpack_from(">b", data_block, 8)[0] + 1
+    
+    return kindList[kind](data_block)
+
+
+def main():
+    f = open(r"C:\Users\user\Desktop\Journal", 'rb')
+    s = f.read()
+    jParseList = journalParser(s)
+    g = open(r"C:\result2.txt", 'w')
+    for i in jParseList:
+        g.write("-----------\n")
+        for j in i:
+            g.write(str(j)+"\n")
+    
+    g.close()
+    f.close()
+
+        
+if __name__ == '__main__':
+    main()
---
+++ Working/HFSPlusJournalAnalyzer_ver2.0_2015_02-16-0117/HFSPlus_getInstance.py
@@ -0,0 +1,337 @@
+from struct import *
+import HFSPlus_sStructure as ss
+
+def getJournalHeader(jh_binary):
+    endian = unpack_from(">I",jh_binary, 4)[0]
+    eflag = [">","<"][endian == 0x78563412]
+    vec = list(unpack_from(eflag + 'IIQQQIII',jh_binary))
+    vec[1] = eflag  # replace 'endian' field as '>' or '<'. It represents endianess more clearly.
+    return ss.JournalHeader(*vec)
+
+def getBlockInfo(bi_binary):
+    vec = unpack_from('<QII',bi_binary)
+    return ss.BlockInfo(*vec)
+
+def getBlockListHeader(blh_binary):
+    blh_0 = blh_binary[:16]
+    blh_1 = blh_binary[16:] 
+    vec = list(unpack('<HHIII',blh_0))
+    vec.append(getBlockInfo(blh_1))
+    return ss.BlockListHeader(*vec)
+
+def getNodeDescriptor(nd_binary):
+    vec = unpack_from('>IIbBHH',nd_binary)
+    return ss.NodeDescriptor(*vec)
+
+def getBTHeaderRec(hr_binary):
+    vec = unpack_from('>HIIIIHHIIHIBBI',hr_binary)  # reserved3 field omited
+    return ss.BTHeaderRec(*vec)
+
+def getExtentDescriptor(ed_binary):
+    vec = unpack_from('>II',ed_binary)
+    return ss.ExtentDescriptor(*vec)
+
+def getForkData(fd_binary):
+    fd_0 = fd_binary[:16]
+    ext = []
+    for i in range(8):
+        temp = fd_binary[16+8*i:16+8*(i+1)]
+        e = getExtentDescriptor(temp)
+        ext.append(e)
+    vec = list(unpack_from('>QII',fd_binary))
+    vec.append(ss.ExtentsDataRec(*ext))
+    return ss.ForkData(*vec)
+
+
+def getBSDInfo(bsd_binary):
+    vec = unpack_from(">IIBBHI", bsd_binary)
+    return ss.BSDInfo(*vec)
+
+def getFolderInfo(foi_binary):
+    RectVec = unpack_from(">hhhh", foi_binary)
+    Flag = unpack_from(">H", foi_binary, 8)[0]
+    Location = unpack_from(">hh", foi_binary, 10)
+    reserved = unpack_from(">H", foi_binary, 14)[0]
+    vec = [ss.Rect(*RectVec), Flag, ss.Point(*Location), reserved]
+    return ss.FolderInfo(*vec)
+
+def getExtendedFolderInfo(efi_binary):
+    scrPosVec = unpack_from(">hh", efi_binary)
+    vec = list(unpack_from(">iHhi", efi_binary, 4))
+    vec.insert(0, ss.Point(*scrPosVec))
+    return ss.ExtendedFolderInfo(*vec)
+
+def getFileInfo(fii_binary):
+    vec = list(unpack_from(">IIH",fii_binary))
+    Location = unpack_from(">hh", fii_binary, 10)
+    res = unpack_from(">H", fii_binary, 14)[0]
+    vec.extend([ss.Point(*Location), res])
+    return ss.FileInfo(*vec)
+
+def getExtendedFileInfo(efi_binary):
+    res1 = unpack_from(">hhhh", efi_binary)
+    vec = list(unpack_from(">Hhi", efi_binary, 8))
+    vec.insert(0, res1)
+    return ss.ExtendedFileInfo(*vec)
+
+def getCatalogFolder(cfo_binary):
+    cfo_0 = cfo_binary[:32]   
+    cfo_1 = cfo_binary[32:48]  # BSDInfo
+    cfo_2 = cfo_binary[48:64]  # userInfo
+    cfo_3 = cfo_binary[64:80]  # finderInfo
+    cfo_4 = cfo_binary[80:]  # textEncoding, reserved 
+    
+    vec0 = list(unpack('>hHIIIIIII',cfo_0))
+    BSDInfo = getBSDInfo(cfo_1)
+    userInfo = getFolderInfo(cfo_2)
+    finderInfo = getExtendedFolderInfo(cfo_3)
+    textEncoding, reserved = unpack_from(">II", cfo_4)
+    vec1 = [BSDInfo, userInfo, finderInfo, textEncoding, reserved]
+    
+    vec0.extend(vec1)
+    return ss.CatalogFolder(*vec0)
+    
+def getCatalogFile(cfi_binary):
+    cfi_0 = cfi_binary[:32]
+    cfi_1 = cfi_binary[32:48]  # BSDInfo
+    cfi_2 = cfi_binary[48:64]  # userInfo
+    cfi_3 = cfi_binary[64:80]  # finderInfo
+    cfi_4 = cfi_binary[80:88]  # textEncoding, reserved 
+    cfi_5 = cfi_binary[88:168] # dataFork
+    cfi_6 = cfi_binary[168:]   # resourceFork
+    
+    vec0 = list(unpack('>hHIIIIIII',cfi_0))
+    BSDInfo = getBSDInfo(cfi_1)
+    userInfo = getFileInfo(cfi_2)
+    finderInfo = getExtendedFileInfo(cfi_3)
+    textEncoding, reserved2 = unpack(">II", cfi_4) 
+    dataFork = getForkData(cfi_5)
+    resourceFork = getForkData(cfi_6)
+    vec1 = [BSDInfo, userInfo, finderInfo, textEncoding, reserved2, dataFork, resourceFork]
+    
+    vec0.extend(vec1)
+    return ss.CatalogFile(*vec0)
+
+def getCatalogThread(cth_binary):
+    cth_0 = cth_binary[:10]
+    cth_1 = cth_binary[10:]
+    recordType, reserved, parID, nameLen = unpack(">hhIH",cth_0)
+    nodeUnicode = "".join(map(unichr, unpack_from(">"+nameLen*"H", cth_1)))
+    
+    return ss.CatalogThread(recordType, reserved, parID, ss.UniChar(nameLen, nodeUnicode))
+    
+def getCatalogKey(ck_binary):
+    rType, parID, nameLen = unpack_from(">HIH", ck_binary)
+    nameStr = ck_binary[8:]
+    nodeUnicode = "".join(map(unichr, unpack_from(">"+nameLen*"H", nameStr)))
+    return ss.CatalogKey(rType, parID, ss.UniChar(nameLen, nodeUnicode))
+
+def getCatalogLeafRec(clk_binary):
+    keyLen = unpack_from(">H", clk_binary)[0]
+    catalKey = getCatalogKey(clk_binary[:2+keyLen])
+    rec_binary = clk_binary[2+keyLen:]
+    recordType = unpack_from(">H", rec_binary)[0]
+    typeList = [0, getCatalogFolder, getCatalogFile, getCatalogThread, getCatalogThread]
+    Record = typeList[recordType](rec_binary)
+    return ss.BTRecord(catalKey, Record)
+
+def getCatalogLeaf(cl_binary):
+    cl_buf = memoryview(cl_binary)
+    nd = getNodeDescriptor(cl_binary)
+    leafRecList = []
+    
+    cl_buf = cl_buf[14:]
+    for i in range(nd.numRecords):
+        lr = getCatalogLeafRec(cl_buf)
+        leafRecList.append(lr)
+        cl_buf = cl_buf[len(lr):]
+        
+    return ss.LeafNode(nd, leafRecList)
+
+def getCatalogPointerRec(cpr_binary):
+    keyLen = unpack_from(">H", cpr_binary)[0]
+    catalKey = getCatalogKey(cpr_binary[:2+keyLen])
+    nodeNum = unpack_from(">I", cpr_binary[2+keyLen:])[0]
+    return ss.BTPointerRec(catalKey, nodeNum)
+
+def getCatalogIndex(ci_binary):
+    ci_buf = memoryview(ci_binary)
+    nd = getNodeDescriptor(ci_buf)
+    offsetList = [14]
+    PointerRecList = []
+    for i in range(nd.numRecords):
+        offset = unpack(">H", ci_buf[-2*i-4:-2*i-2])[0]
+        offsetList.append(offset)
+    for i in range(len(offsetList)-1):
+        temp = ci_buf[offsetList[i]:offsetList[i+1]]
+        cpr = getCatalogPointerRec(temp)
+        PointerRecList.append(cpr)
+    return ss.IndexNode(nd,PointerRecList)
+
+'''
+Extents
+'''
+def getExtentsKey(ek_binary):
+    vec = unpack_from(">HBBII", ek_binary)
+    return ss.ExtentsKey(*vec)
+
+def getExtentsLeafRec(elf_binary):
+    extKey = getExtentsKey(elf_binary[:12])
+    rec_binary = elf_binary[12:]
+    ext = []
+    for i in range(8):
+        temp = rec_binary[16+8*i:16+8*(i+1)]
+        e = getExtentDescriptor(temp)
+        ext.append(e)
+    
+    return ss.BTRecord(extKey, ss.ExtentsDataRec(*ext))
+
+def getExtentsLeaf(el_binary):
+    el_buf = memoryview(el_binary)
+    nd = getNodeDescriptor(el_binary)
+    leafRecList = []
+    el_buf = el_buf[14:]
+    for i in range(nd.numRecords):
+        lr = getExtentsLeafRec(el_buf)
+        leafRecList.append(lr)
+        el_buf = el_buf[len(lr):]
+    return ss.LeafNode(nd, leafRecList)
+
+def getExtentsPointerRec(epr_binary):
+    extKey = getExtentsKey(epr_binary[:12])
+    nodeNum = unpack_from(">I", epr_binary[12:])[0]
+    return ss.BTPointerRec(extKey, nodeNum)
+
+def getExtentsIndex(ei_binary):
+    ei_buf = memoryview(ei_binary)
+    nd = getNodeDescriptor(ei_buf)
+    offsetList = [14]
+    PointerRecList = []
+    for i in range(nd.numRecords):
+        offset = unpack(">H", ei_buf[-2*i-4:-2*i-2])[0]
+        offsetList.append(offset)
+    for i in range(len(offsetList)-1):
+        temp = ei_buf[offsetList[i]:offsetList[i+1]]
+        epr = getExtentsPointerRec(temp)
+        PointerRecList.append(epr)
+    return ss.IndexNode(nd,PointerRecList)
+
+'''
+Attribute
+'''
+
+def getAttributesKey(ak_binary):
+    vec = list(unpack_from(">HHIIH", ak_binary))
+    nameLen = vec[-1]
+    nameUni = "".join(map(unichr, unpack_from(">"+nameLen*"H", ak_binary[14:])))
+    vec.append(nameUni)
+    return ss.AttrKey(*vec)
+
+def getAttributesData(ad_binary):
+    vec = list(unpack_from(">IQI", ad_binary))
+    attrSize = vec[-1]
+    attrData = ad_binary[16:16+attrSize+(attrSize%2)]
+    vec.append(attrData)
+    return ss.AttrData(*vec)
+
+def getAttributesForkData(af_binary):
+    recordType, res = unpack_from(">II", ad_binary)
+    fd = getForkData(af_binary[8:])
+    return ss.AttrForkData(recordType, res, fd)
+
+def getAttributesExtents(ae_binary):
+    recordType, res = unpack_from(">II", ad_binary)
+    rec_binary = ad_binary[8:]
+    ext = []
+    for i in range(8):
+        temp = rec_binary[16+8*i:16+8*(i+1)]
+        e = getExtentDescriptor(temp)
+        ext.append(e)
+    return ss.AttrExtents(recordType, res, ss.ExtentsDataRec(*ext))
+
+def getAttributesLeafRec(alr_binary):
+    keyLen = unpack_from(">H", alr_binary)[0]
+    attKey = getAttributesKey(alr_binary[:2+keyLen])
+    rec_binary = alr_binary[2+keyLen:]
+    recordType = unpack_from(">I", rec_binary)[0]
+    typeList = [0, getAttributesData, getAttributesForkData, getAttributesExtents]
+    Record = typeList[recordType/0x10](rec_binary)
+    return ss.BTRecord(attKey, Record)
+
+def getAttributesLeaf(al_binary):
+    al_buf = memoryview(al_binary)
+    nd = getNodeDescriptor(al_binary)
+    leafRecList = []
+    
+    al_buf = al_buf[14:]
+    for i in range(nd.numRecords):
+        lr = getAttributesLeafRec(al_buf)
+        leafRecList.append(lr)
+        al_buf = al_buf[len(lr):]
+    
+    return ss.LeafNode(nd, leafRecList)
+
+def getAttributesPointerRec(apr_binary):
+    keyLen = unpack_from(">H", apr_binary)[0]
+    attKey = getAttributesKey(apr_binary[:2+keyLen])
+    nodeNum = unpack_from(">I", apr_binary[2+keyLen:])[0]
+    return ss.BTPointerRec(attKey, nodeNum)
+
+def getAttributesIndex(ai_binary):
+    ai_buf = memoryview(ai_binary)
+    nd = getNodeDescriptor(ai_buf)
+    offsetList = [14]
+    PointerRecList = []
+    for i in range(nd.numRecords):
+        offset = unpack(">H", ai_buf[-2*i-4:-2*i-2])[0]
+        offsetList.append(offset)
+    for i in range(len(offsetList)-1):
+        temp = ai_buf[offsetList[i]:offsetList[i+1]]
+        apr = getAttributesPointerRec(temp)
+        PointerRecList.append(apr)
+    return ss.IndexNode(nd,PointerRecList)
+
+'''
+Header, Map
+'''
+
+def getHeaderNode(ch_binary): 
+    ch_buf = memoryview(ch_binary)
+    nd = getNodeDescriptor(ch_buf)
+    hr = getBTHeaderRec(ch_buf[14:])
+    ch_buf = ch_buf[120:]
+    udr = ch_buf[:128]
+    mr = ch_buf[128:-8]
+    return ss.HeaderNode(nd, hr, udr, mr)
+
+def getMapNode(mn_binary):
+    mn_buf = memoryview(mn_binary)
+    nd = getNodeDescriptor(mn_buf)
+    offsetList = [14]
+    for i in range(nd.numRecords):
+        offset = unpack(">H", mn_buf[-2*i-4:-2*i-2])[0]
+        offsetList.append(offset)
+    mr = mn_buf[14:offsetList[1]]
+    return ss.MapNode(nd, mr)
+
+'''
+VolumeHeader
+'''
+    
+def getVolumeHeader(vh_binary):
+    vh_0 = vh_binary[:80]
+    vh_1 = vh_binary[80:112]
+    vh_sp = []
+    for i in range(5):
+        t = vh_binary[112+80*i:112+80*(i+1)]
+        vh_sp.append(t)
+        
+    vec = list(unpack('>HHIIIIIIIIIIIIIIIIIQ',vh_0))
+    
+    fI = unpack('>IIIIIIII',vh_1)
+    vec.append(fI)
+    
+    for i in range(5):
+        vec.append(getForkData(vh_sp[i]))
+    return ss.VolumeHeader(*vec)
+    
