ae4ef7cbdb89017f822a72ac6944f5f11fc5f8bd
hyunhochoi <factorial__@naver.com>
2015-02-18 22:01:26 +0900

    Revert "1"
    This reverts commit 8033cb874921207edfc91b93846e4022cd7f42d5.

---
+++ Working/HFSPlusJournalAnalyzer_ver2.0_2015_02-16-2249/HFSPlus_PatternIdentifiy.py
@@ -0,0 +1,17 @@
+'''
+Created on 2015. 2. 16.
+
+@author: biscuit
+'''
+from HFSPlus_JournalTrack import *
+from HFSPlus_ParseModule import *
+
+class JournalPattern:
+    pass
+
+def refineTrack(journalTrack):
+    pass
+
+def inspectPattern(journalTrack):
+    #for tBucket in journalTrack:
+    pass
---
+++ Working/HFSPlusJournalAnalyzer_ver2.0_2015_02-16-2249/Output.py
@@ -0,0 +1,125 @@
+from sqlalchemy import *
+'''
+engine=create_engine('raw.db')
+
+CatalogBase=declarative_base()
+ExtentsBase=declarative_base()
+AttrBase=declarative_base()
+'''
+class CatalogLeaf(CatalogKey,CatalogFile,CatalogFolder,CatalogThread):
+
+    __slots__=()
+
+    for i in CatalogLeaf._fields:
+        CatalogLeaf
+
+
+    def __init__(self,cdr):
+
+        for i in CatalogLeaf._fields:
+            self.__dict__[i]=''
+
+        if cdr.record.recordType<3:
+
+            for i in cdr.key.__dict__:
+                self.__dict__[i]=cdr.key.__dict__[i]
+
+        else:
+            self.CNID=cdr.key.parentID
+
+        for i in cdr.record.__dict__:
+            self.__dict__[i]=cdr.record.__dict__[i]
+
+
+class ExtentsLeaf(ExtentsKey,ExtentsDataRec):
+
+    __slots__=()
+
+    def __init__(self,edr):
+
+        for i in edr.key.__dict__:
+            self.__dict__[i]=edr.key.__dict__[i]
+
+        for i in edr.record.__dict__:
+            self.__dict__[i]=edr.record.__dict__[i]
+
+
+class AttrLeaf(AttrBase,AttrKey,AttrForkData,AttrExtents,AttrData):
+
+    __slots__=()
+
+    def __init__(self,adr):
+
+        for i in adr.key.__dict__:
+            self.__dict__[i]=adr.key.__dict__[i]
+
+        for i in adr.record.__dict__:
+            self.__dict__[i]=adr.record.__dict__[i]
+
+
+def temp(path,jParseList):
+
+    f = open("{0}/result1.txt".format(path),'w')
+        for i in jParseList:
+            f.write("-----------\n")
+            for j in i:
+                f.write(str(j)+"\n")
+        
+        f.close()
+
+
+def volumeInfo(path,vh):
+
+    if vh!=0:
+        f = open("{0}/VolumeInfo.txt".format(path),'w')
+        for i in vh.__dict__:
+            f.write('{0} : {1}\n'.format(i,vh.__dict__[i]))
+    
+    f.close()
+
+
+def outputNode(f,node):
+
+    for i in range(len(node)):
+
+            try:
+    
+                for j in range(len(node[i].LeafRecList)):
+
+                    index=node[i].LeafRecList[j].getType()
+                    lf=BTLeafType[index](node[i].LeafRecList[j])
+
+                    for k in lf.__dict__:
+                        f[index].write(str(lf.__dict__[k]).replace(',',' ')+',')
+
+                    f[index].write('\n')
+
+            except AttributeError:
+                pass
+
+
+def rawcsv(path,jParseList):
+
+    BTLeafType={'Catalog':CatalogLeaf,"Extents":ExtentsLeaf,"Attributes":AttrLeaf}
+
+    for i in BTType:
+        f.append(open('{0}/{1}.csv'.format(path,i),'w'))
+
+    f={}
+    for i in ['Catalog','Extents','Attributes']:
+        for j in BTLeafType[i]._fields:
+            f[i].write('{0},'.format(j))
+        f[i].write('\n')
+
+    for i in range(1,len(jParseList)):
+        outputNode(jParseList[i][2])
+
+    for i in ['Catalog','Extents','Attributes']:
+        f.close(f[i])
+
+
+#def rawSQLite3(path,jParseList):
+
+
+
+    
