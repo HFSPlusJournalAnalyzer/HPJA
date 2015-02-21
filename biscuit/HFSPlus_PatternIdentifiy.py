09713c23e6797fb289e065bfb9b8835e4f117a18
biscuit03 <biscuit@kaist.ac.kr>
2015-02-21 22:22:04 +0900

    sdasd
    asdasdasd

--- biscuit/HFSPlus_PatternIdentifiy.py
+++ biscuit/HFSPlus_PatternIdentifiy.py
@@ -5,9 +5,71 @@ Created on 2015. 2. 16.
 '''
 from HFSPlus_JournalTrack import *
 from HFSPlus_ParseModule import *
+from collections import namedtuple
+
+CatalogPatVector = namedtuple("CatalogPatVector", ['contModDate', 'attrModDate', 'accessDate', 
+                                                   'logicalSize', 'totalBlocks',
+                                                   'remove', 'insert',
+                                                   'thID'])
+
+
 
 class JournalPattern:
-    pass
+    def __init__(self, patternName, pattern):
+        self.patternName = patternName
+        self.pattern = CatalogPatVector(*pattern)
+        
+    def __eq__(self, other):
+        return self.pattern == other.pattern
+    
+    def hasPattern(self, dataChange):
+        return self.pattern == getCatalPattern(dataChange)
+
+PatternSet = [JournalPattern('Insert', [0,0,0,0,0,0,1,0]),
+              JournalPattern('Remove', [0,0,0,0,0,1,0,0]),
+              JournalPattern('Read/Modify', [1,1,1,0,0,0,0,0]),
+              JournalPattern('Read/Modify', [1,1,1,0,1,0,0,0]),
+              JournalPattern('Read/Modify', [1,1,1,0,-1,0,0,0]),
+              JournalPattern('Modify(no size change)', [1,1,0,0,0,0,0,0]),
+              JournalPattern('Modify(size extended)', [1,1,0,1,1,0,0,0]),
+              JournalPattern('Modify(size extended)', [1,1,0,1,0,0,0,0]),
+              JournalPattern('Read', [0,0,1,0,0,0,0,0]),
+              JournalPattern('Read/Modify', [0,0,1,-2,-2,0,0,0])
+              ]    
+
+def getCatalPattern(CataldataChange):
+    initvec = [0]*8
+    setCatPatVect(CataldataChange, initvec)
+    return CatalogPatVector(*initvec)
+
+def setCatPatVect(change, catVector):
+    if change.__class__ == renewalChangeInfo:
+        dateType = ['contentModDate', 'attributeModDate', 'accessDate']
+        if change.__name__ in dateType:
+            catVector[dateType.index(change.__name__)] = 1
+        return
+    if change.__class__ == objectChangeInfo:
+        if change.chType == 'Insert':
+            catVector[6] = 1
+        if change.chType == 'Remove':
+            catVector[5] = 1
+        return
+    if change.__class__ == valueChangeInfo:
+        if change.__name__ == 'logicalSize':
+            if change.diff != 0:
+                catVector[3] = change.diff / abs(change.diff)
+            if change.after == 0:
+                catVector[3] = -2
+        if change.__name__ == 'totalBlocks':
+            if change.diff != 0:
+                catVector[4] = change.diff / abs(change.diff)
+            if change.after == 0:
+                catVector[4] = -2
+        if change.__name__ == "parentID":
+            catVector[7] = 1
+        return
+    for i in change.chList:
+        setCatPatVect(i, catVector)
 
 def refineTrack(journalTrack):
     refPass1 = []
@@ -21,7 +83,7 @@ def refineTrack(journalTrack):
         temp = [j for j in i if j.type == "CatalogFile"]
         if temp != []:
             refPass2.append(temp)
-    
+            
     return refPass2
 
 def catalogGrouping(refineCatalog):
@@ -64,6 +126,7 @@ def inspectPattern(journalTrack):
     pass
 
 def main():
+    global PatternSet
     f = open(r"C:\Users\user\Desktop\Journal_2", 'rb')
     s = f.read()
     jPList, pInfo = journalParser(s)[:2]
@@ -73,12 +136,25 @@ def main():
     
     g = open(r"C:\TEMP\result_2.txt", 'w')
     for i,j in enumerate(c):
-        g.write("%d=====\n"%i)
+        g.write("%d====\n" %i)
         for x, k in j.iteritems():
-            g.write("%d---\n" %x)
             for p in k:
-                g.write("%s" %p)
-    
+                a = 0
+                for Pat in PatternSet:
+                    if Pat.hasPattern(p):
+                        try:
+                            g.write("[%s] %s\n" %(Pat.patternName, p.nData))
+                            a = 1
+                            break
+                        except(AttributeError):
+                            g.write("[%s] %s\n" %(Pat.patternName, p.absData))
+                            a = 1
+                            break
+                if a == 0:
+                    print "Unknown"
+                    print p
+                    print '---------'
+                
     
     f.close()
     
